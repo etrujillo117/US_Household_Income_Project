0e3320400c7e45217005b7ae9a8f0f57397fe8e4
Edgar Trujillo <67111396+etrujillo117@users.noreply.github.com>
2024-05-25 16:12:06 -0400

    Update US Household Income EDA.sql
    Updated table names

--- scripts/US Household Income EDA.sql
+++ scripts/US Household Income EDA.sql
@@ -1,16 +1,16 @@
 #US Household Income EDA
 
 #Initial Table views
-SELECT * FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income`
+SELECT * FROM us_household_income
 ;
 
-SELECT * FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics`
+SELECT * FROM us_household_statistics
 ;
 
 # Explore the Area of Land and Water date by State
 #Top 15 states by area of land
 SELECT State_Name, SUM(ALand) AS State_ALand, SUM(AWater) AS State_AWater
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income`
+FROM us_household_income
 GROUP BY State_Name
 ORDER BY 2 DESC
 LIMIT 15
@@ -18,7 +18,7 @@ LIMIT 15
 
 #Top 15 states by area of water
 SELECT State_Name, SUM(ALand) AS State_ALand, SUM(AWater) AS State_AWater
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income`
+FROM us_household_income
 GROUP BY State_Name
 ORDER BY 3 DESC
 LIMIT 15
@@ -26,16 +26,16 @@ LIMIT 15
 
 #Combine the data from both tables to see what other interesting findings we may have
 SELECT *
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 ;
 
 #What are the 5 Lowest Average Income States according to the data set?
 SELECT u.State_Name, ROUND(AVG(Mean),1) as AVG_Mean, ROUND(AVG(Median),1) as AVG_Median
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 GROUP BY u.State_Name
@@ -45,8 +45,8 @@ LIMIT 5
 
 #What are the 5 Highest Average Income States according to the data set?
 SELECT u.State_Name, ROUND(AVG(Mean),1) as AVG_Mean, ROUND(AVG(Median),1) as AVG_Median
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 GROUP BY u.State_Name
@@ -56,8 +56,8 @@ LIMIT 5
 
 # What Type has the Highest Average income?
 SELECT Type, COUNT(Type) as type_entries, ROUND(AVG(Mean),1) as AVG_Mean, ROUND(AVG(Median),1) as AVG_Median
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 GROUP BY Type
@@ -67,8 +67,8 @@ Limit 1
 
 # City level Average Income
 SELECT u.State_Name, City, ROUND(AVG(Mean),1) as AVG_Mean
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 GROUP BY u.State_Name, City
@@ -77,8 +77,8 @@ ORDER By 3 DESC
 
 # Puerto Rico Average Income by City
 SELECT u.State_Name, City, ROUND(AVG(Mean),1) as AVG_Mean
-FROM `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Income` as u
-INNER JOIN `bigquery-public-data-313800.AnalystBuilderMySQLCourse.US_Household_Statistics` as us
+FROM us_household_income as u
+INNER JOIN us_household_statistics as us
   ON u.id = us.id
 WHERE Mean <> 0
 AND u.State_Name = 'Puerto Rico'
